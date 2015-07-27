eca890471d01a243929561fd104377f5b0a4d004
Jonathan Sauvé <jonathansauve3@gmail.com>
2015-07-27 08:02:30 -0400

    add xmlManagerTest package

---
+++ tmf/org.eclipse.tracecompass.tmf.analysis.xml.ui.tests/src/org/eclipse/tracecompass/tmf/analysis/xml/ui/tests/xmlManager/XmlManagerViewerTest.java
@@ -0,0 +1,32 @@
+package org.eclipse.tracecompass.tmf.analysis.xml.ui.tests.xmlManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+
+/**
+ * Test suite for the XmlManagerViewer class.
+ */
+@SuppressWarnings("javadoc")
+public class XmlManagerViewerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
